79a4d843d2529be267a091bb5ca9faf7ea3cfb5a
michael <loganmitchellahaha@gmail.com>
2023-02-15 10:08:45 +0200

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"ecmaVersion": 5
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"warn",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
